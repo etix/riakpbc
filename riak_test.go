c1037f67c8ef0c2212994e2dde8e79c69c9a6720
mrb <michaelrbernstein@gmail.com>
2012-05-14 19:24:21 -0400

    add bench

--- riak_test.go
+++ riak_test.go
@@ -1,12 +1,19 @@
 package riakpbc
 
 import (
-	"github.com/bmizerany/assert"
-	"io/ioutil"
-	"log"
-	"os"
+  "mrb/riakpbc"
 	"testing"
 )
 
-func TestAsserts(t *testing.T) {
+func BenchmarkRead(b *testing.B) {
+  b.N = 10000
+	riak, err := riakpbc.Dial("127.0.0.1:8081")
+
+	if err != nil {
+	  return
+  }
+
+	for i := 0; i < b.N; i++ {
+		_, _ = riak.FetchObject("bucket", "keyzles")
+	}
 }
